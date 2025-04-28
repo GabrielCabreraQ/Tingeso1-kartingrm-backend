f22effa76c3b857527949398db11bc73e109af43
Gabriel Cabrera <gabriel.cabrera.q@usach.cl>
2025-04-28 11:07:35 -0400

    DB-dump.sql create database

--- DB-dump.sql
+++ DB-dump.sql
@@ -16,6 +16,31 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+
+
+--
+-- Name: kartingrm; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE "kartingrm" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE "kartingrm" OWNER TO postgres;
+
+\connect -reuse-previous=on "dbname='kartingrm'"
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
